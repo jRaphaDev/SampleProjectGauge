8a1114744b7fbb06eb1c2acae7c656a2bd96270c
Raphael Freitas <dev.rapha@gmail.com>
2022-08-25 00:41:49 -0300

    modify Implementation

--- tests/CalculatorStepImplementation.cs
+++ tests/CalculatorStepImplementation.cs
@@ -8,7 +8,7 @@ namespace SampleProjectGauge
 {
 	public class CalculatorStepImplementation
 	{
-        private readonly Calculator _calculator = new Calculator();
+        private readonly Calculator _calculatorApp = new Calculator();
         private int _result;
 
 		[Step("Then the result should be <result>.")]
@@ -21,19 +21,19 @@ namespace SampleProjectGauge
 		[Step("Given the first number is <number>.")]
 		public void GivenTheFirstNumberIs(int number)
 		{
-            _calculator.FirstNumber = number;
+            _calculatorApp.FirstNumber = number;
 		}
 	
 		[Step("And the second number is <number>.")]
 		public void AndTheSecondNumberIs(int number)
 		{
-            _calculator.SecondNumber = number;
+            _calculatorApp.SecondNumber = number;
 		}
 	
 		[Step("When the two numbers are added.")]
 		public void WhenTheTwoNumbersAreAdded()
 		{
-            _result = _calculator.Add();
+            _result = _calculatorApp.Add();
 		}
 	}
 }
